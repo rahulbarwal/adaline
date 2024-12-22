4b9ba226f38f0fd9c441357f9aa2c4f1363ccb79
Rahul Barwal <rahul.barwal@appsmith.com>
2024-12-22 19:38:58 +0530

    Basic server setup.

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,14 @@
+import cors from "cors";
+import express from "express";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
